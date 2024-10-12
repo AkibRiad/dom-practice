48407b9d4b503ee4ff5088f24d3099d04a7dc9d7
AkibRiad <akibriad652@gmail.com>
2024-10-13 00:34:26 +0600

    third push

--- js/index.js
+++ js/index.js
@@ -1,29 +1,35 @@
 
 
-document.getElementById('btn').addEventListener('click',function(){
-    const textT = document.getElementById('textContainer');
-    const getText = textT.value;
-    const main=document.getElementById('main-container');
+// document.getElementById('btn').addEventListener('click',function(){
+//     const textT = document.getElementById('textContainer');
+//     const getText = textT.value;
+//     const main=document.getElementById('main-container');
 
-    const p=document.createElement('p');
-    p.innerText=getText;
-    main.appendChild(p);
-    textT.value='';
+//     const p=document.createElement('p');
+//     p.innerText=getText;
+//     main.appendChild(p);
+//     textT.value='';
 
-})
+// })
 
-    document.getElementById('input-text').addEventListener('keyup',function(event){
-        const keyOpen= document.getElementById('input-text');
-        const textt= keyOpen.value;
-        const text = document.getElementById('btntwo');
-        if(textt === 'delete'){
-            text.removeAttribute('disabled');
-        }
-         else{
-            text.setAttribute('disabled',true);
-         }                                           
+//     document.getElementById('input-text').addEventListener('keyup',function(event){
+//         const keyOpen= document.getElementById('input-text');
+//         const textt= keyOpen.value;
+//         const text = document.getElementById('btntwo');
+//         if(textt === 'delete'){
+//             text.removeAttribute('disabled');
+//         }
+//          else{
+//             text.setAttribute('disabled',true);
+//          }                   
+//     })
 
+    
+    document.getElementById('item-2').addEventListener('click',function(event){
+        console.log(7);
+        event.stopPropagation();
     })
 
-
-
+document.getElementById('list-ul').addEventListener('click',function(){
+    console.log(100);
+})
